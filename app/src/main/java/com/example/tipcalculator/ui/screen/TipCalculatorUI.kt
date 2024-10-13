7c9f6ad19002aea5dfa93e14ebd2327cafd770b6
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-10-13 10:31:18 -0500

    Input Text Field Completed

--- app/src/main/java/com/example/tipcalculator/ui/screen/TipCalculatorUI.kt
+++ app/src/main/java/com/example/tipcalculator/ui/screen/TipCalculatorUI.kt
@@ -1,5 +1,6 @@
 package com.example.tipcalculator.ui.screen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,27 +9,32 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tipcalculator.components.InputField
 
 @Composable
 fun TipCalculatorUI( content:@Composable () -> Unit = {}) {
-    val totalPerPerson = remember{ mutableStateOf(0.0) }
+    val totalPerPerson =  remember { mutableStateOf(0.0) }
+    val totalBil = remember{ mutableStateOf(0.0) }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 35.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 35.dp, start = 8.dp, end = 8.dp)
     ) {
         TopHeader()
     }
@@ -67,6 +73,37 @@ fun TopHeader(totalPerPerson: Double = 0.0, updateTotal: (Double) -> (Unit) = {}
 
 @Composable
 @Preview
-fun mainContent() {
+fun MainContent() {
+    val totalBillState = remember{mutableStateOf("")}
+    val validState = remember(totalBillState.value){
+        totalBillState.value.trim().isNotEmpty()
+    }
 
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 3.dp, color = Color.LightGray)
+    ) {
+
+        Column() {
+
+            InputField(valueState = totalBillState ,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+                    //TODO - onvalue changed
+
+                    keyboardController?.hide()
+                }
+            )
+
+        }
+
+    }
 }
